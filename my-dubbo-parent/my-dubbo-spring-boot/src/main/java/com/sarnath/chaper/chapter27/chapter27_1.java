11bedd1bc52b91524912d352a01f2cc75803a2fc
GitHubProfessor <liuhaibo_ljf@163.com>
2018-03-06 17:06:25 +0800

    add chapter20,29 delete chapter20

--- my-dubbo-parent/my-dubbo-spring-boot/src/main/java/com/sarnath/chaper/chapter27/chapter27_1.java
+++ my-dubbo-parent/my-dubbo-spring-boot/src/main/java/com/sarnath/chaper/chapter27/chapter27_1.java
@@ -1,67 +1,67 @@
-package com.sarnath.chaper.chapter27;
-
-
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * 27.1 The “Spring Web MVC Framework”
- * 
- * 访问 http://localhost:8089/users/1  进入
- * 
- * @author OM
- *
- */
-@RestController
-@RequestMapping(value="/users")
-public class chapter27_1 {
-	@RequestMapping(value="/{user}", method=RequestMethod.GET)
-	public void getUser(@PathVariable Long user) {
-		String str = String.format("@PathVariable user :%s", user);
-		System.out.println("@PathVariable user :" + user);
-	}
-
-	@RequestMapping(value="/{user}/customers", method=RequestMethod.GET)
-	void getUserCustomers(@PathVariable Long user) {
-		System.out.println("@PathVariable user :" + user);
-	}
-
-	@RequestMapping(value="/{user}", method=RequestMethod.DELETE)
-	public void deleteUser(@PathVariable Long user) {
-		System.out.println("@PathVariable user :" + user);
-	}
-	
-	/**
-	 * 27.1.11 Error Handling
-	 * 此方法结合下面的方法，用来演示如果自定义错误信息
-	 * 此方法抛出异常，然后会返回500错误码，然后下面的方法注册了500错误码对应的页面，
-	 * 所以500错误都会用我们自定义的500.html页面来显示
-	 */
-	@RequestMapping(value="/errorpage", method=RequestMethod.GET)
-	public void errorPage() {
-		throw new RuntimeException();
-	}
-	
-	/**
-	 * 这个bean用来配置自定义错误页面的。上面errorpage报异常，返回状态码500后，
-	 * 这个类便可以捕捉到500，然后跳转到指定的页面，显示自定义的500错误的页面
-	 * @return
-	 
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-
-	   return (container -> {
-	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/errorpage/401.html");
-	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404.html");
-	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/errorpage/500.html");
-
-	        container.addErrorPages(error401Page, error404Page, error500Page);
-	   });
-	}*/
-}
-
+//package com.sarnath.chaper.chapter27;
+//
+//
+//import org.springframework.boot.web.server.ErrorPage;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * 27.1 The “Spring Web MVC Framework”
+// * 
+// * 访问 http://localhost:8089/users/1  进入
+// * 
+// * @author OM
+// *
+// */
+//@RestController
+//@RequestMapping(value="/users")
+//public class chapter27_1 {
+//	@RequestMapping(value="/{user}", method=RequestMethod.GET)
+//	public void getUser(@PathVariable Long user) {
+//		String str = String.format("@PathVariable user :%s", user);
+//		System.out.println("@PathVariable user :" + user);
+//	}
+//
+//	@RequestMapping(value="/{user}/customers", method=RequestMethod.GET)
+//	void getUserCustomers(@PathVariable Long user) {
+//		System.out.println("@PathVariable user :" + user);
+//	}
+//
+//	@RequestMapping(value="/{user}", method=RequestMethod.DELETE)
+//	public void deleteUser(@PathVariable Long user) {
+//		System.out.println("@PathVariable user :" + user);
+//	}
+//	
+//	/**
+//	 * 27.1.11 Error Handling
+//	 * 此方法结合下面的方法，用来演示如果自定义错误信息
+//	 * 此方法抛出异常，然后会返回500错误码，然后下面的方法注册了500错误码对应的页面，
+//	 * 所以500错误都会用我们自定义的500.html页面来显示
+//	 */
+//	@RequestMapping(value="/errorpage", method=RequestMethod.GET)
+//	public void errorPage() {
+//		throw new RuntimeException();
+//	}
+//	
+//	/**
+//	 * 这个bean用来配置自定义错误页面的。上面errorpage报异常，返回状态码500后，
+//	 * 这个类便可以捕捉到500，然后跳转到指定的页面，显示自定义的500错误的页面
+//	 * @return
+//	 
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//
+//	   return (container -> {
+//	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/errorpage/401.html");
+//	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404.html");
+//	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/errorpage/500.html");
+//
+//	        container.addErrorPages(error401Page, error404Page, error500Page);
+//	   });
+//	}*/
+//}
+//
--- my-dubbo-parent/my-dubbo-spring-boot/src/main/java/com/sarnath/chaper/chapter27/chapter27_4_4/ProgrammaticCustomization.java
+++ my-dubbo-parent/my-dubbo-spring-boot/src/main/java/com/sarnath/chaper/chapter27/chapter27_4_4/ProgrammaticCustomization.java
@@ -1,51 +1,51 @@
-package com.sarnath.chaper.chapter27.chapter27_4_4;
-
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-/**
- * 27.4.4 Customizing Embedded Servlet Containers
- * 
- * 通过编码来设置tomcat等container配置
- * 
- * 方法一：实现WebServerFactoryCustomizer接口，然后通过customize方法设置
- * 方法二：通过@Bean注册一个spring bean，然后在方法中设置。
- * 
- * 当感觉方法一有所局限时，可选择使用方法二。
- * 
- * 此类只有在spring boot2.0下才好用，当前是1.5.9.所以注释掉了
- *
- */
-public class ProgrammaticCustomization implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>{
-
-	/**
-	 * 方法一
-	 */
-	@Override
-	public void customize(ConfigurableServletWebServerFactory server) {
-		server.setPort(9000);
-	}
-	
-	
-	/**
-	 * 方法二
-	 */
-	@Bean
-	public ConfigurableServletWebServerFactory webServerFactory() {
-		TomcatServletWebServerFactory server = new TomcatServletWebServerFactory();
-		Session session = new Session();
-		session.setTimeout(Duration.ofHours(1));
-		server.setPort(9000);
-		server.setSession(session);
-		server.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
-		return server;
-	}
-}
+//package com.sarnath.chaper.chapter27.chapter27_4_4;
+//
+//
+//import java.time.Duration;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.ErrorPage;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+//import org.springframework.boot.web.servlet.server.Session;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpStatus;
+///**
+// * 27.4.4 Customizing Embedded Servlet Containers
+// * 
+// * 通过编码来设置tomcat等container配置
+// * 
+// * 方法一：实现WebServerFactoryCustomizer接口，然后通过customize方法设置
+// * 方法二：通过@Bean注册一个spring bean，然后在方法中设置。
+// * 
+// * 当感觉方法一有所局限时，可选择使用方法二。
+// * 
+// * 此类只有在spring boot2.0下才好用，当前是1.5.9.所以注释掉了
+// *
+// */
+//public class ProgrammaticCustomization implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>{
+//
+//	/**
+//	 * 方法一
+//	 */
+//	@Override
+//	public void customize(ConfigurableServletWebServerFactory server) {
+//		server.setPort(9000);
+//	}
+//	
+//	
+//	/**
+//	 * 方法二
+//	 */
+//	@Bean
+//	public ConfigurableServletWebServerFactory webServerFactory() {
+//		TomcatServletWebServerFactory server = new TomcatServletWebServerFactory();
+//		Session session = new Session();
+//		session.setTimeout(Duration.ofHours(1));
+//		server.setPort(9000);
+//		server.setSession(session);
+//		server.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+//		return server;
+//	}
+//}
